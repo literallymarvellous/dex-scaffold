ef7adb934e0ab40629dbcc0c8c868af4eb794492
literallymarvellous <ahiaraikechukwu@gmail.com>
2022-08-14 20:25:28 +0100

    edited readme

--- packages/hardhat/deploy/00_deploy_your_contract.js
+++ packages/hardhat/deploy/00_deploy_your_contract.js
@@ -50,7 +50,7 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   await balloons.approve(dex.address, ethers.utils.parseEther("100"));
   console.log("INIT exchange...");
   await dex.init(ethers.utils.parseEther("5"), {
-    value: ethers.utils.parseEther("5"),
+    value: ethers.utils.parseEther("0.1"),
     gasLimit: 200000,
   });
 };
--- packages/react-app/src/constants.js
+++ packages/react-app/src/constants.js
@@ -1,5 +1,5 @@
 // MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
-export const INFURA_ID = "7b0e75d38d424750b92791477924d133";
+export const INFURA_ID = "e5f57e3684bf43068b71f7a25887cd46";
 
 // MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
 export const ETHERSCAN_KEY = "DNXJA8RX2Q3VZ4URQIWP7Z68CJXQZSC6AW";
